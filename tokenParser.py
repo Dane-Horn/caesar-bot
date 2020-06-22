374207da515551af0067c4fa625bc3809b480227
Dane Horn <dane.horn7915@gmail.com>
2020-06-22 13:43:09 +0200

    add display to parser

--- tokenParser.py
+++ tokenParser.py
@@ -13,18 +13,22 @@ mul_ops = {
 def disadvantage(state, out, rolls, base):
     rolls = [randint(1, base) for _ in range(rolls)]
     value = min(rolls)
-    output(state, out, f'({value})')
+    output(state, out, f'({stringify_rolls(rolls)} = {value})')
     return value
 def advantage(state, out, rolls, base):
     rolls = [randint(1, base) for _ in range(rolls)]
     value = max(rolls)
-    output(state, out, f'({value})')
+    output(state, out, f'({stringify_rolls(rolls)} = {value})')
     return value
 functions = {
     'dis': disadvantage,
     'adv': advantage
 }
 
+def stringify_rolls(rolls):
+    if len(rolls) > 100:
+        return '...'
+    return ', '.join(map(str, rolls))
 
 def expect(state, types):
     found = curr_type(state) in types
@@ -60,9 +64,17 @@ def parse(s):
         "tokens": tokenize(s),
         "output": []
     }
-    value = expression(state)
-    print(value)
-    print(state["output"])
+    value = roll_expression(state)
+    print(s)
+    result = f'{" ".join(state["output"])} = {value}'
+    print(result)
+    return result
+
+def roll_expression(state, out=True):
+    expect(state, {'function', 'die', 'lparen', 'number', 'variable'})
+    value = expression(state, out)
+    expect(state, {'eof'})
+    return value
 
 # expression = func_term
 def expression(state, out=True):
@@ -77,6 +89,7 @@ def add_term(state, out=True):
     value = mul_term(state, out)
     while match(state, {'add_op'}):
         op = curr_value(state)
+        output(state, out, op)
         next_token(state)
         second_value = mul_term(state, out)
         value = add_ops[op](value, second_value)
@@ -99,39 +112,47 @@ def die_term(state, out=True):
     expect(state, {'function', 'lparen', 'die', 'number', 'variable'})
     if match(state, {'die'}):
         next_token(state)
-        base = factor(state, out)
+        base = factor(state, False)
         value = randint(1, base)
+        output(state, out, f'{ {value} }')
     else:
         value = factor(state, out)
         if match(state, {'die'}):
             next_token(state)
-            base = factor(state, out)
-            value = sum(randint(1, base) for _ in range(value))
+            base = factor(state, False)
+            rolls = [randint(1, base) for _ in range(value)]
+            value = sum(rolls)
+            if out:
+                state["output"].pop()
+            output(state, out, f'{{{stringify_rolls(rolls)} = {value}}}')
     return value
 
-# factor     = "("expression")"|function"("expression{","expression}")"|number|variable
+# factor     = "("expression")"|function"("[expression{","expression}"])"|number|variable
 # ignoring variable functionality for now
 def factor(state, out=True):
     expect(state, {'function', 'lparen', 'number'})
     if match(state, {'lparen'}):
         next_token(state)
-        value = expression(state, out)
+        value = expression(state, False)
         expect(state, {'rparen'})
         next_token(state)
+        output(state, out, f'({value})')
     elif match(state, {'function'}):
         func = curr_value(state)
         next_token(state)
         expect(state, {'lparen'})
         next_token(state)
         args = [state, out]
-        args.append(expression(state, out))
-        while match(state, {"comma"}):
-            next_token(state)
-            args.append(expression(state, out))
+        if not match(state, "rparen"):
+            args.append(expression(state, False))
+            while match(state, {"comma"}):
+                next_token(state)
+                args.append(expression(state, False))
         value = functions[func](*args)
         expect(state, {'rparen'})
         next_token(state)
     else:
         value = int(curr_value(state))
+        output(state, out, f'{value}')
         next_token(state)
     return value
