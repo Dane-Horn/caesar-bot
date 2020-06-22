5031e41890f71ab56e05c0003e7e8a0d145a9549
Dane Horn <dane.horn7915@gmail.com>
2020-06-22 09:16:45 +0200

    allow tokenize to take any dictionary of token definitions

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import re
 
-tokens = {
+default_tokens = {
     "number": r'\d+',
     "add_op": r'\+|-',
     "mul_op": r'\*|/',
@@ -12,8 +12,8 @@ tokens = {
     "unknown": r'.+'
 }
 
-mapped_regex = map(lambda kv: f'(?P<{kv[0]}>{kv[1]})', tokens.items())
-combined = re.compile('|'.join(mapped_regex))
-def tokenize(s):
+def tokenize(s, tokens=default_tokens):
+    mapped_regex = map(lambda kv: f'(?P<{kv[0]}>{kv[1]})', tokens.items())
+    combined = re.compile('|'.join(mapped_regex))
     matches = [{k: m[k] for k in m.groupdict() if m[k] is not None} for m in re.finditer(combined, s)]
     return matches
