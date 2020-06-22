e6d842613b25ab94b253952008a4753725108650
Dane Horn <dane.horn7915@gmail.com>
2020-06-22 10:53:45 +0200

    update ebnf and expected sets

--- tokenParser.py
+++ tokenParser.py
@@ -30,26 +30,32 @@ def parse(s):
     }
 
 def expression(state):
+    expect(state, 'function', 'paren', 'number', 'variable')
     pass
 
 def func_term(state):
+    expect(state, 'function', 'die', 'paren', 'number', 'variable')
     pass
 
 def add_term(state):
+    expect(state, 'paren', 'die', 'number', 'variable')
     pass
 
 def mul_term(state):
+    expect(state, 'paren', 'die', 'number', 'variable')
     pass
 
 def die_term(state):
+    expect(state, 'paren', 'die', 'number', 'variable')
     pass
 
 def factor(state):
+    expect(state, 'paren', 'number', 'variable')
     pass
 
 # expression = func_term eof
 # func_term  = function"("expression")"|add_term
 # add_term   = mul_term{add_op mul_term}
 # mul_term   = die_term{mul_op die_term}
-# die_term   = factor["d"factor]
+# die_term   = factor["d"factor]|"d"factor
 # factor     = "("expression")"|number|variable
