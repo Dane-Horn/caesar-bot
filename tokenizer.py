48ed8359d56c3b75e570154d76c9e3a985561418
Dane Horn <dane.horn7915@gmail.com>
2020-06-22 11:10:14 +0200

    change tokenization process to return type, value dictionaries

--- tokenizer.py
+++ tokenizer.py
@@ -9,11 +9,16 @@ default_tokens = {
     "die": r'd',
     "paren": r'\(|\)',
     "comma": r',',
-    "unknown": r'.+'
+    "unknown": r'.+?'
 }
+
+def map_match(m):
+    filtered_m = {k: m[k] for k in m.groupdict() if m[k] is not None} 
+    key, value = next(iter(filtered_m.items()))
+    return {"type": key, "value": value}
 
 def tokenize(s, tokens=default_tokens):
     mapped_regex = map(lambda kv: f'(?P<{kv[0]}>{kv[1]})', tokens.items())
     combined = re.compile('|'.join(mapped_regex))
-    matches = [{k: m[k] for k in m.groupdict() if m[k] is not None} for m in re.finditer(combined, s)]
+    matches = [map_match(m) for m in re.finditer(combined, s)]
     return matches
