6c48ce1b6b2b18161a502929c7673caee8a47ff8
Dane Horn <dane.horn7915@gmail.com>
2020-06-22 08:52:13 +0200

    build regex groups dynamically

--- tokenizer.py
+++ tokenizer.py
@@ -1,18 +1,19 @@
 import re
 
 tokens = {
-    "number": r'(?P<number>\d+)',
-    "add_op": r'(?P<add_op>\+|-)',
-    "mul_op": r'(?P<mul_op>\*|/)',
-    "function": r'(?P<function>dis|adv)',
-    "die": r'(?P<die>d)',
-    "paren": r'(?P<paren>\(|\))',
-    "comma": r'(?P<comma>,)',
-    "unknown": r'(?P<unknown>.+)'
+    "number": r'\d+',
+    "add_op": r'\+|-',
+    "mul_op": r'\*|/',
+    "variable": r'\$[a-zA-Z]+',
+    "function": r'dis|adv',
+    "die": r'd',
+    "paren": r'\(|\)',
+    "comma": r',',
+    "unknown": r'.+'
 }
 
-
-combined = re.compile('|'.join(tokens.values()))
+mapped_regex = map(lambda kv: f'(?P<{kv[0]}>{kv[1]})', tokens.items())
+combined = re.compile('|'.join(mapped_regex))
 def tokenize(s):
     matches = [{k: m[k] for k in m.groupdict() if m[k] is not None} for m in re.finditer(combined, s)]
     return matches
