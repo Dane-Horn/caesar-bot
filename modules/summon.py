ee12455e87fbdd22c1db524f293ff38f152e3bd1
Dane Horn <dane.horn7915@gmail.com>
2020-11-12 19:15:16 +0200

    fix summon

--- modules/summon.py
+++ modules/summon.py
@@ -4,5 +4,5 @@ from bot import bot  # pylint: disable=no-name-in-module
 
 
 @bot.command('summon')
-async def pokerole(ctx, n):
-    await ctx.send(f'{ctx.author.mention} you have no power here!')
+async def summon(ctx, n):
+    await ctx.send(f'{ctx.author.name}, you have no power here!')
